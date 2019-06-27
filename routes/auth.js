3a83eec4c1ab90aab6123f364b5004ca4939fc23
mung9 <cutiemung2@naver.com>
2019-06-28 01:50:05 +0900

    dummy

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {User} = require('../models/user');
+const { User } = require("../models/user");
 
-const authService = require('../services/auth');
-const generateColor = require('../utils/color');
+// const authService = require("../services/auth");
+const generateColor = require("../utils/color");
 
-const words = ["강아지", "망아지", "앵무새", "비둘기", "개미", "지렁이", "칠면조", "벌"];
-function generateWord(){
-  return '익명의 '+words[Math.floor(Math.random()%3)];
+const words = [
+  "강아지",
+  "망아지",
+  "앵무새",
+  "비둘기",
+  "개미",
+  "지렁이",
+  "칠면조",
+  "벌"
+];
+function generateWord() {
+  return "익명의 " + words[Math.floor(Math.random() % 3)];
 }
 
-router.post('/login', async (req,res)=>{
+router.post("/login", async (req, res) => {
   const username = req.body.username;
-  let user = await User.findOne({username});
-  if(!user){
+  let user = await User.findOne({ username });
+  if (!user) {
     const colorcode = generateColor();
     const nickname = generateWord();
-    user = await (new User({username, colorcode, nickname})).save();
+    user = await new User({ username, colorcode, nickname }).save();
     console.log(user);
-  } 
+  }
 
   console.log(`login success: ${user.nickname}`);
   return res.status(200).send({
@@ -29,12 +38,13 @@ router.post('/login', async (req,res)=>{
   });
 });
 
-router.post('/logout', async (req,res)=>{
-  try{
-  await User.findByIdAndDelete(req.body.id);
-  } catch(err){
-    res.status(400).send('비정상 요청');
+router.post("/logout", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.body.id);
+    console.log(`${user.nickname} 로그아웃`);
+  } catch (err) {
+    res.status(400).send("비정상 요청");
   }
-})
+});
 
 module.exports = router;
