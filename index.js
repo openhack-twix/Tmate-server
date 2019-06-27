0c60dbf7822ccf62cefc99599ecaa34c868d6ec6
mung9 <cutiemung2@naver.com>
2019-06-28 05:53:40 +0900

    이모!

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const logger = require('./logger');
+const logger = require("./logger");
 const express = require("express");
 const app = express();
 const initRoom = require("./rooms");
 const http = require("http");
 const server = http.createServer(app);
-const mongoose = require('mongoose');
-const helmet = require('helmet')
+const mongoose = require("mongoose");
+const helmet = require("helmet");
 app.use(helmet());
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -17,7 +17,7 @@ app.use(express.json());
 
 initRoom(server);
 
-require('./middlewares/routes')(app);
+require("./middlewares/routes")(app);
 
 // if(!process.env.jwtKey){
 //   console.error('jwtKey is not defined');
@@ -27,16 +27,20 @@ require('./middlewares/routes')(app);
 // database 연결
 const dbUrl = process.env.DB_URL || "mongodb://localhost/tmatet";
 
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
 mongoose
-  .connect(dbUrl, {
-    useNewUrlParser: true,
-    useFindAndModify: false
-  })
-  
-  .then(() => logger.info("🔥 Connected to mongodb!",`[${dbUrl}]`))
-  .catch(err => logger.info(`☠️ Failed to connect to mongodb: [${dbUrl}]`, err.message));
+  .connect(dbUrl)
+  .then(() => logger.info("🔥 Connected to mongodb!", `[${dbUrl}]`))
+  .catch(err =>
+    logger.info(`☠️ Failed to connect to mongodb: [${dbUrl}]`, err.message)
+  );
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   logger.info(`🌈 start listening on port ${port}`);
 });
+
+logger.info("💩 🔥️ 🌈 😜 ⭐ ❌ 🐕")
