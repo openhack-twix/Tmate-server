bf6b5efb6046d403f78e4d25b4abb4caac8736a6
mung9 <cutiemung2@naver.com>
2019-06-28 02:18:32 +0900

    불필요한 파일, 코드 제거

--- middlewares/routes.js
+++ middlewares/routes.js
@@ -1,12 +1,7 @@
-// const auth = require('../routes/auth');
-
-// const user = require('../routes/user');
 const auth = require('../routes/auth');
 const room = require('../routes/room');
 
 module.exports = exports = function(app) {
-  // app.use('/api/auth',auth);
-  // app.use('/api/users',user);
   app.use('/auth',auth);
   app.use('/api/rooms',room);
 }
--- middlewares/session.js
+++
@@ -1,9 +0,0 @@
-const session = require('express-session');
-const sessionKey = require('../secure/session_key.json');
-
-module.exports = session({
-  secret: sessionKey.key,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-});
